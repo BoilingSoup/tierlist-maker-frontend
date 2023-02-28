bc17443cb4865d74749b41798cc31540b54e65d6
BoilingSoup <boilingsoup@protonmail.com>
2023-02-28 00:56:15 -0800

    Add mantine and placeholder front page

---
+++ components/common/styles.ts
@@ -0,0 +1,12 @@
+import { CSSObject } from "@mantine/core";
+
+export const navbarSx = (): CSSObject => ({
+  backgroundColor: "black",
+  height: "52px",
+  justifyContent: "space-between",
+});
+
+export const navTextSx = (): CSSObject => ({
+  color: "white",
+  margin: "auto 0",
+});
