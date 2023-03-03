d1a1cd4cbe69cdd03a0253b1b9b2457ef9b2b6e3
BoilingSoup <boilingsoup@protonmail.com>
2023-03-03 15:06:39 -0800

    Add logo and hamburger menu to navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,14 +1,44 @@
-import { Flex, Group, Text } from "@mantine/core";
-import { navbarSx, navTextSx } from "./styles";
+import { Flex, Group, Text, Burger, MediaQuery } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { logoImg } from "../../config/config";
+import {
+  displayNone,
+  logoFlexSx,
+  logoTextSx,
+  navbarSx,
+  navLinkTextSx,
+} from "./styles";
+
+const navPadding = 25;
 
 export const Navbar = () => {
+  const [opened, { toggle }] = useDisclosure(false);
+  const label = opened ? "Close navigation" : "Open navigation";
+
   return (
-    <Flex sx={navbarSx} pl={25}>
-      <Text sx={navTextSx}>Tierlist.lol</Text>
-      <Group pr={25}>
-        <Text sx={navTextSx}>Browse</Text>
-        <Text sx={navTextSx}>Register</Text>
-        <Text sx={navTextSx}>Login</Text>
+    <Flex sx={navbarSx} pl={navPadding}>
+      <Flex sx={logoFlexSx}>
+        <img src={logoImg} width="50" height="40" />
+        <Text sx={logoTextSx} component="h1">
+          Tierlist.lol
+        </Text>
+      </Flex>
+      <Group pr={navPadding}>
+        <MediaQuery styles={displayNone} largerThan="sm">
+          <Burger
+            color="white"
+            opened={opened}
+            onClick={toggle}
+            aria-label={label}
+          />
+        </MediaQuery>
+        <MediaQuery styles={displayNone} smallerThan="sm">
+          <Group>
+            <Text sx={navLinkTextSx}>Browse</Text>
+            <Text sx={navLinkTextSx}>Register</Text>
+            <Text sx={navLinkTextSx}>Login</Text>
+          </Group>
+        </MediaQuery>
       </Group>
     </Flex>
   );
---
+++ config/config.ts
@@ -0,0 +1,2 @@
+/** logo image src path, relative to /public directory */
+export const logoImg = "./trollol.png";
