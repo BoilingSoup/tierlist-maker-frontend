a43bab6109969df1c246d58feffd26d1e854c31e
BoilingSoup <84747244+BoilingSoup@users.noreply.github.com>
2023-03-02 19:16:52 -0800

    Add browse page boilerplate (#5)
    * Update css drawing styles
    
    * Add browse page boilerplate

---
+++ pages/browse/index.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../../components/common/Navbar";
+import type { NextPage } from "next";
+
+const Browse: NextPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Browse page...</div>
+    </>
+  );
+};
+
+export default Browse;
