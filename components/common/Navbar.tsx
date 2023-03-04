cf2c722143164c59bc473a30bbfb9d9e608f309d
BoilingSoup <boilingsoup@protonmail.com>
2023-03-04 12:17:29 -0800

    Use window resize event to conditionally close mobile menu

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,6 +1,15 @@
-import { Flex, Group, Text, Burger, MediaQuery, Box } from "@mantine/core";
+import {
+  Flex,
+  Group,
+  Text,
+  Burger,
+  MediaQuery,
+  Box,
+  useMantineTheme,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { LOGO_IMG } from "../../config/config";
+import { useCloseHamburgerOnWindowResize } from "./hooks/useCloseMobileMenuIfLargeScreen";
 import { LogoLink } from "./LogoLink";
 import { MobileMenu } from "./MobileMenu";
 import { displayNone, navbarHeight, navbarSx, navLinkTextSx } from "./styles";
@@ -8,6 +17,17 @@ import { displayNone, navbarHeight, navbarSx, navLinkTextSx } from "./styles";
 export const Navbar = () => {
   const [opened, { toggle }] = useDisclosure(false);
   const label = opened ? "Close navigation" : "Open navigation";
+  const { breakpoints } = useMantineTheme();
+
+  /**
+   * Auto-close hamburger menu if window gets bigger than Mantine's sm breakpoint.
+   */
+  useCloseHamburgerOnWindowResize({
+    opened,
+    toggle,
+    debounceMs: 20,
+    breakpoint: breakpoints.sm,
+  });
 
   return (
     <>
@@ -37,6 +57,11 @@ export const Navbar = () => {
         </Group>
       </Flex>
       {opened && <MobileMenu />}
+
+      {/**
+       * Blank Box to offset all contents below Navbar because Navbar is position="fixed"
+       * (Navbar stays at top while scrolling but is not part of regular document flow.)
+       **/}
       <Box pt={navbarHeight} />
     </>
   );
