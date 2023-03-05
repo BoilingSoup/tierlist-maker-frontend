eea67ce77c2bd92b0950b81c4a1eca11b28b966f
BoilingSoup <boilingsoup@protonmail.com>
2023-03-04 17:10:14 -0800

    Create foundation for mobile menu

--- components/common/MobileMenu.tsx
+++ components/common/MobileMenu.tsx
@@ -1,20 +1,66 @@
-import { Box } from "@mantine/core";
+import {
+  Box,
+  Center,
+  CSSObject,
+  Flex,
+  MantineTheme,
+  Text,
+} from "@mantine/core";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { navbarHeight } from "./styles";
 
+const linkCss = ({ colors }: MantineTheme): CSSObject => ({
+  color: "white",
+  height: "70px",
+  backgroundColor: "black",
+  "&:hover": {
+    backgroundColor: colors.cyan[8],
+  },
+});
+
 export const MobileMenu = () => {
+  {
+    /* TODO: highlight the currently active route with a different color/marker */
+  }
+  const router = useRouter();
+
   return (
     <Box
       sx={{
         position: "fixed",
-        zIndex: 100,
+        zIndex: 1,
         color: "white",
         backgroundColor: "black",
         width: "100%",
         marginTop: navbarHeight,
-        height: `calc(100% - ${navbarHeight})`,
+        height: `calc(100vh - ${navbarHeight})`,
+        alignItems: "center",
       }}
     >
-      Mobile Menu
+      <Flex sx={{ flexDirection: "column" }}>
+        <Link style={{ width: "100%", textDecoration: "none" }} href="/">
+          <Center sx={linkCss}>
+            <Text size="xl">Home</Text>
+          </Center>
+        </Link>
+        <Link style={{ width: "100%", textDecoration: "none" }} href="/browse">
+          <Center sx={linkCss}>
+            <Text size="xl">Browse</Text>
+          </Center>
+        </Link>
+        <Link style={{ width: "100%", textDecoration: "none" }} href="/create">
+          <Center sx={linkCss}>
+            <Text size="xl">Create Tier List</Text>
+          </Center>
+        </Link>
+        <Center sx={linkCss}>
+          <Text size="xl">Sign In</Text>
+        </Center>
+        <Center sx={linkCss}>
+          <Text size="xl">Register</Text>
+        </Center>
+      </Flex>
     </Box>
   );
 };
