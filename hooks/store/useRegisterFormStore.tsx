81b211c76b3520da8062f7e6e099f885c91ec92d
BoilingSoup <boilingsoup@protonmail.com>
2023-03-12 16:13:23 -0700

    Add tests for register form validations

--- hooks/store/useRegisterFormStore.tsx
+++ hooks/store/useRegisterFormStore.tsx
@@ -4,18 +4,24 @@ import { RegisterFormFields, RegisterFormValues } from "../auth/types";
 type RegisterFormStore = {
   values: RegisterFormValues;
   update(data: RegisterFormUpdate): void;
+  reset(): void;
 };
 
 type RegisterFormUpdate = { input: RegisterFormFields; value: string };
 
+export const initialValues: RegisterFormValues = {
+  email: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
+
 export const useRegisterFormStore = create<RegisterFormStore>((set) => ({
-  values: {
-    email: "",
-    username: "",
-    password: "",
-    confirmPassword: "",
-  },
+  values: { ...initialValues },
   update: ({ input, value }: RegisterFormUpdate) => {
     set((state) => ({ values: { ...state.values, [input]: value } }));
+  },
+  reset: () => {
+    set((state) => ({ ...state, values: initialValues }));
   },
 }));
