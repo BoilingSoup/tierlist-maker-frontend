d326beeeef9a61c9fd986cf1a4b267715193a936
BoilingSoup <84747244+BoilingSoup@users.noreply.github.com>
2023-03-22 10:07:06 -0700

    Update styles (#62)
    * Update styles
    
    * Update buttons
    
    * Refactor and style

---
+++ components/tierlist/Sidebar.tsx
@@ -0,0 +1,170 @@
+import {
+  Box,
+  Button,
+  Center,
+  CSSObject,
+  FileButton,
+  Flex,
+  Image as MantineImage,
+  List,
+  MantineTheme,
+  Text,
+} from "@mantine/core";
+import {
+  IconClipboard,
+  IconDeviceFloppy,
+  IconDownload,
+  IconMaximize,
+  IconMaximizeOff,
+  IconPlus,
+  IconWorldUpload,
+} from "@tabler/icons-react";
+import { Dispatch, RefObject, SetStateAction, useRef } from "react";
+import { useClientSideImageID } from "../../hooks/store/useClientSideImageID";
+import {
+  actionButtonsGroupSx,
+  actionButtonsSx,
+  addImageButtonsAreaSx,
+  addImageButtonSx,
+  imageAreaContainerSx,
+  imageAreaMaxBoundsSx,
+  imagesFlexContainerSx,
+  scrollContainerSx,
+  sidebarContainerSx,
+} from "./styles";
+import { ClientSideImage } from "./types";
+
+type FileButtonProps = {
+  resetRef: RefObject<() => void>;
+  onChange: (files: File[]) => void;
+  accept: string;
+  multiple: boolean;
+};
+
+type Props = {
+  imageSources: ClientSideImage[];
+  onAddImage: Dispatch<SetStateAction<ClientSideImage[]>>;
+  fullScreen: {
+    state: boolean;
+    toggle: () => Promise<void>;
+  };
+};
+
+export const Sidebar = ({
+  imageSources,
+  onAddImage: setImageSources,
+  fullScreen,
+}: Props) => {
+  const resetRef = useRef<() => void>(null);
+  const { toggle: toggleFullScreen, state: isFullScreen } = fullScreen;
+
+  const addFileHandler = (files: File[]) => {
+    const newImages: ClientSideImage[] = files.map((file) => ({
+      id: useClientSideImageID.getState().getID(),
+      src: URL.createObjectURL(file),
+    }));
+    console.log(newImages);
+    setImageSources((prev) => [...prev, ...newImages]);
+
+    if (resetRef.current !== null) {
+      resetRef.current();
+    }
+  };
+
+  const fileButtonProps: FileButtonProps = {
+    resetRef: resetRef,
+    onChange: addFileHandler,
+    accept: "image/png,image/jpeg,image/webp",
+    multiple: true,
+  };
+
+  return (
+    <Flex sx={sidebarContainerSx}>
+      <Box sx={imageAreaMaxBoundsSx}>
+        <Flex sx={imageAreaContainerSx}>
+          <Center sx={scrollContainerSx}>
+            <Flex sx={imagesFlexContainerSx}>
+              {!imageSources.length && (
+                <Center
+                  sx={{
+                    height: "100%",
+                    width: "100%",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Text component="h2" sx={{ fontSize: "1.8rem" }}>
+                    Add Images Here!
+                  </Text>
+                  <br />
+                  <List
+                    sx={{
+                      color: "white",
+                      fontSize: "1.3rem",
+                    }}
+                    styles={{ itemWrapper: { marginTop: "30px" } }}
+                  >
+                    <List.Item>Copy/Paste images or URLs</List.Item>
+                    <List.Item>Use the button below to upload files</List.Item>
+                  </List>
+                </Center>
+              )}
+              {imageSources.map((img) => (
+                <Center
+                  key={img.id}
+                  sx={{
+                    width: "100px",
+                    height: "100px",
+                    overflow: "hidden",
+                    border: "2px solid white",
+                    margin: "1px",
+                  }}
+                >
+                  <MantineImage
+                    src={img.src}
+                    // width={100}
+                    // height={100}
+                    sx={{ height: "auto", width: "100px" }}
+                  />
+                </Center>
+              ))}
+            </Flex>
+          </Center>
+          <Flex sx={addImageButtonsAreaSx}>
+            <FileButton {...fileButtonProps}>
+              {(props) => (
+                <Button {...props} color="dark.7" sx={addImageButtonSx}>
+                  <IconPlus /> Add File
+                </Button>
+              )}
+            </FileButton>
+            <Button color="dark.7" sx={addImageButtonSx}>
+              <IconClipboard /> Paste
+            </Button>
+          </Flex>
+        </Flex>
+      </Box>
+      <Flex sx={actionButtonsGroupSx}>
+        <Center component="button" sx={actionButtonsSx}>
+          <IconDownload />
+          <Text>Export PNG</Text>
+        </Center>
+        <Center
+          component="button"
+          sx={actionButtonsSx}
+          onClick={toggleFullScreen}
+        >
+          {isFullScreen ? <IconMaximizeOff /> : <IconMaximize />}
+          <Text>{/*isFullscreen ? "Exit" : "Enter"*/} Full Screen</Text>
+        </Center>
+        <Center component="button" sx={actionButtonsSx}>
+          <IconDeviceFloppy />
+          <Text>Save</Text>
+        </Center>
+        <Center component="button" sx={actionButtonsSx}>
+          <IconWorldUpload />
+          <Text>Publish</Text>
+        </Center>
+      </Flex>
+    </Flex>
+  );
+};
--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,36 +1,19 @@
+import { Box, Flex } from "@mantine/core";
 import {
-  Box,
-  Button,
-  Center,
-  CSSObject,
-  FileButton,
-  Flex,
-  Image as MantineImage,
-  List,
-  Text,
-  Tooltip,
-} from "@mantine/core";
-import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
-import { useFullscreen, useViewportSize } from "@mantine/hooks";
-import {
-  IconDeviceFloppy,
-  IconDownload,
-  IconHelpCircle,
-  IconMaximize,
-  IconMaximizeOff,
-  IconWorldUpload,
-} from "@tabler/icons-react";
+  useFullscreen as useFullScreen,
+  useViewportSize,
+} from "@mantine/hooks";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { RefObject, useRef, useState } from "react";
+import { useState } from "react";
 import { NAVBAR_HEIGHT } from "../../components/common/styles";
 import { usePasteEvent } from "../../components/tierlist/hooks/usePasteEvent";
+import { Sidebar } from "../../components/tierlist/Sidebar";
 import { TierListRow } from "../../components/tierlist/TierListRow";
 import {
   ClientSideImage,
   TierListRowProps,
 } from "../../components/tierlist/types";
-import { useClientSideImageID } from "../../hooks/store/useClientSideImageID";
 
 const initialData: (Omit<TierListRowProps, "height"> & { key: number })[] = [
   { key: 1, color: "#fe7f7f", label: "S", items: [] },
@@ -40,42 +23,20 @@ const initialData: (Omit<TierListRowProps, "height"> & { key: number })[] = [
   { key: 5, color: "#7fbfff", label: "D", items: [] },
 ];
 
-const buttonsSx: CSSObject = {
-  width: "50%",
-  height: "50%",
-  display: "inline",
-};
-
-const buttonsContainer: CSSObject = {
-  width: "100%",
-  height: "20%",
-};
-
 const Create: NextPage = () => {
-  const {
-    toggle: toggleFullscreen,
-    fullscreen,
-    ref: fullscreenRef,
-  } = useFullscreen();
-
+  const fullScreen = useFullScreen();
+  const isFullScreen = fullScreen.fullscreen;
+  //
   //
   const [data, setData] = useState<typeof initialData>(initialData);
   const { height } = useViewportSize();
-  const rowHeight = fullscreen
+  const rowHeight = isFullScreen
     ? `${height / data.length}px`
     : `${(height - +NAVBAR_HEIGHT.split("px").shift()!) / data.length}px`;
 
   const [imageSources, setImageSources] = useState<Array<ClientSideImage>>([]);
 
-  /**
-   *
-   * Move these later
-   *
-   */
-
   usePasteEvent(setImageSources);
-
-  const resetRef = useRef<() => void>(null);
 
   return (
     <>
@@ -83,7 +44,7 @@ const Create: NextPage = () => {
         <title>Create Tier List</title>
       </Head>
       <Flex
-        ref={fullscreenRef}
+        ref={fullScreen.ref}
         sx={{
           width: "100%",
           height: `calc(100vh - ${NAVBAR_HEIGHT})`,
@@ -106,137 +67,14 @@ const Create: NextPage = () => {
             />
           ))}
         </Box>
-        <Box
-          sx={(theme) => ({
-            width: "25%",
-            backgroundColor: theme.colors.dark[4],
-            color: "white",
-          })}
-        >
-          <Flex
-            sx={(theme) => ({
-              height: "80%",
-              background: theme.colors.dark[4],
-            })}
-          >
-            <Flex
-              sx={{
-                height: "95%",
-                width: "95%",
-                margin: "auto",
-                border: "2px solid orange",
-                borderRadius: "8px",
-                flexDirection: "column",
-              }}
-            >
-              <Center
-                sx={{
-                  height: "90%",
-                  width: "100%",
-                }}
-              >
-                <Flex
-                  sx={{
-                    height: "95%",
-                    width: "95%",
-                    flexWrap: "wrap",
-                    alignContent: "flex-start",
-                    overflow: "auto",
-                  }}
-                >
-                  {!imageSources.length && (
-                    <Center
-                      sx={{
-                        height: "100%",
-                        width: "100%",
-                        flexDirection: "column",
-                      }}
-                    >
-                      <Text component="h2" sx={{ fontSize: "1.8rem" }}>
-                        Add Images Here!
-                      </Text>
-                      <br />
-                      <List
-                        sx={{
-                          color: "white",
-                          fontSize: "1.3rem",
-                        }}
-                        styles={{ itemWrapper: { marginTop: "30px" } }}
-                      >
-                        <List.Item>Copy/Paste images or URLs</List.Item>
-                        <List.Item>
-                          Use the button below to upload files
-                        </List.Item>
-                      </List>
-                    </Center>
-                  )}
-                  {imageSources.map((img) => (
-                    <Center
-                      key={img.id}
-                      sx={{
-                        width: "100px",
-                        height: "100px",
-                        overflow: "hidden",
-                        border: "2px solid white",
-                        margin: "1px",
-                      }}
-                    >
-                      <MantineImage
-                        src={img.src}
-                        // width={100}
-                        // height={100}
-                        sx={{ height: "auto", width: "100px" }}
-                      />
-                    </Center>
-                  ))}
-                </Flex>
-              </Center>
-              <Center sx={{ height: "10%" }}>
-                <FileButton
-                  resetRef={resetRef}
-                  onChange={(files) => {
-                    const newImages: ClientSideImage[] = files.map((file) => ({
-                      id: useClientSideImageID.getState().getID(),
-                      src: URL.createObjectURL(file),
-                    }));
-                    console.log(newImages);
-                    setImageSources((prev) => [...prev, ...newImages]);
-
-                    if (resetRef.current !== null) {
-                      resetRef.current();
-                    }
-                  }}
-                  accept="image/png,image/jpeg,image/webp"
-                  multiple
-                >
-                  {(props) => <Button {...props}>Upload image</Button>}
-                </FileButton>
-              </Center>
-            </Flex>
-          </Flex>
-          <Box sx={buttonsContainer}>
-            <Center component="button" sx={buttonsSx}>
-              <IconDownload />
-              <Text>Export PNG</Text>
-            </Center>
-            <Center
-              component="button"
-              sx={buttonsSx}
-              onClick={toggleFullscreen}
-            >
-              {fullscreen ? <IconMaximizeOff /> : <IconMaximize />}
-              <Text>{fullscreen ? "Exit" : "Enter"} Full Screen</Text>
-            </Center>
-            <Center component="button" sx={buttonsSx}>
-              <IconDeviceFloppy />
-              <Text>Save</Text>
-            </Center>
-            <Center component="button" sx={buttonsSx}>
-              <IconWorldUpload />
-              <Text>Publish</Text>
-            </Center>
-          </Box>
-        </Box>
+        <Sidebar
+          fullScreen={{
+            state: isFullScreen,
+            toggle: fullScreen.toggle,
+          }}
+          imageSources={imageSources}
+          onAddImage={setImageSources}
+        />
       </Flex>
     </>
   );
