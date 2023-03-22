189eb580d248558c9158b2cac63471a28c75ef0f
BoilingSoup <boilingsoup@protonmail.com>
2023-03-22 10:06:23 -0700

    Refactor and style

---
+++ components/tierlist/styles.ts
@@ -0,0 +1,61 @@
+import { CSSObject, MantineTheme } from "@mantine/core";
+
+export const sidebarContainerSx = ({ colors }: MantineTheme): CSSObject => ({
+  width: "25%",
+  backgroundColor: colors.dark[6],
+  color: "white",
+  flexDirection: "column",
+});
+
+export const imageAreaMaxBoundsSx = (): CSSObject => ({
+  height: "85%",
+  width: "100%",
+  flexDirection: "column",
+  position: "relative",
+});
+
+export const imageAreaContainerSx = ({ colors }: MantineTheme): CSSObject => ({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  height: "92%",
+  width: "92%",
+  border: `2px solid ${colors.dark[2]}`,
+  borderRadius: "8px",
+  flexDirection: "column",
+});
+
+export const scrollContainerSx = (): CSSObject => ({
+  height: "100%",
+  width: "100%",
+  overflow: "auto",
+});
+
+export const imagesFlexContainerSx = (): CSSObject => ({
+  height: "96%",
+  width: "96%",
+  flexWrap: "wrap",
+  alignContent: "flex-start",
+});
+
+export const addImageButtonsAreaSx = (): CSSObject => ({ width: "100%" });
+
+export const addImageButtonSx = ({ colors }: MantineTheme): CSSObject => ({
+  width: "50%",
+  border: `1px solid ${colors.dark[3]}`,
+});
+
+export const actionButtonsGroupSx = (): CSSObject => ({
+  width: "100%",
+  height: "15%",
+  flexWrap: "wrap",
+});
+
+export const actionButtonsSx = (theme: MantineTheme): CSSObject => ({
+  width: "50%",
+  height: "50%",
+  color: "white",
+  backgroundColor: theme.colors.dark[5],
+  borderRadius: "8px",
+});
