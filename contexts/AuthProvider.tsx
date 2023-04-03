1ef46ab39b86c0facd8ccd232d5f3645ff0e231c
BoilingSoup <boilingsoup@protonmail.com>
2023-04-03 15:06:24 -0700

    Create Auth context

---
+++ contexts/AuthProvider.tsx
@@ -0,0 +1,44 @@
+import {
+  useContext,
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+} from "react";
+
+type User = {
+  id: string;
+  username: string;
+  is_admin: boolean;
+} | null;
+
+type UserContext = {
+  user: User;
+  setUser: Dispatch<SetStateAction<User>>;
+};
+
+const AuthContext = createContext<UserContext | undefined>(undefined);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+
+  return context;
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+export const AuthProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<User>(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
