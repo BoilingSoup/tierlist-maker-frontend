fd070df6678e07e4da3903201ff86a22746b8db3
BoilingSoup <boilingsoup@protonmail.com>
2023-04-16 11:45:04 -0700

    Update msw handler

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,13 +1,20 @@
 import { rest } from "msw";
 import { User } from "../contexts/AuthProvider";
 
-const mockUser: User = { username: "bobby", id: "1234-5678", is_admin: false };
+const mockUser: User = {
+  username: "bobby",
+  id: "1234-5678",
+  is_admin: false,
+  email: "bobby@bobby.com",
+  email_verified: false,
+  oauth_provider: null,
+};
 
 export const handlers = [
   rest.post("/login", (_, res, ctx) => {
     return res(ctx.status(200), ctx.json<User>(mockUser));
   }),
-  rest.get("/user", (_, res, ctx) => {
+  rest.get("/v1/user", (_, res, ctx) => {
     return res(ctx.status(200), ctx.json<User>(mockUser));
   }),
   rest.get("/sanctum/csrf-cookie", (_, res, ctx) => {
