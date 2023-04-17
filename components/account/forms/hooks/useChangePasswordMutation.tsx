ccf6ad4174acdcac7f0e37e3deacaa732aae38e5
BoilingSoup <boilingsoup@protonmail.com>
2023-04-17 13:14:13 -0700

    Add success notifications

--- components/account/forms/hooks/useChangePasswordMutation.tsx
+++ components/account/forms/hooks/useChangePasswordMutation.tsx
@@ -1,14 +1,23 @@
+import { useMantineTheme } from "@mantine/core";
 import { useMutation } from "react-query";
 import { apiClient } from "../../../../lib/apiClient";
+import { showSuccessNotification } from "../../../common/helpers";
 import { ChangePasswordFormValues } from "../types";
 
 export const useChangePasswordMutation = (formReset: () => void) => {
+  const theme = useMantineTheme();
+
   return useMutation(
     (values: ChangePasswordFormValues) => attemptChangePassword(values),
     {
       onSuccess: () => {
-        // TODO: show success notification & clear form inputs
         formReset();
+
+        showSuccessNotification({
+          theme,
+          title: "Success",
+          message: "Your password was changed.",
+        });
       },
       onError: (err) => {
         // TODO: show error message on inputs or notification
