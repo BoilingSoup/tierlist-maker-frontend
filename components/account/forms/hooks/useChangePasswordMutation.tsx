f1c7380e25773a6049d55b38dd879414a1e317ff
BoilingSoup <boilingsoup@protonmail.com>
2023-04-18 13:21:53 -0700

    Show error when change password fails due to invalid current password

--- components/account/forms/hooks/useChangePasswordMutation.tsx
+++ components/account/forms/hooks/useChangePasswordMutation.tsx
@@ -1,17 +1,24 @@
 import { useMantineTheme } from "@mantine/core";
+import { AxiosError } from "axios";
 import { useMutation } from "react-query";
 import { apiClient } from "../../../../lib/apiClient";
-import { showSuccessNotification } from "../../../common/helpers";
+import {
+  showSomethingWentWrongNotification,
+  showSuccessNotification,
+} from "../../../common/helpers";
 import { ChangePasswordFormValues } from "../types";
+import { useChangePasswordForm } from "./useChangePasswordForm";
 
-export const useChangePasswordMutation = (formReset: () => void) => {
+export const useChangePasswordMutation = (
+  form: ReturnType<typeof useChangePasswordForm>
+) => {
   const theme = useMantineTheme();
 
   return useMutation(
     (values: ChangePasswordFormValues) => attemptChangePassword(values),
     {
       onSuccess: () => {
-        formReset();
+        form.reset();
 
         showSuccessNotification({
           theme,
@@ -19,9 +26,19 @@ export const useChangePasswordMutation = (formReset: () => void) => {
           message: "Your password was changed.",
         });
       },
-      onError: (err) => {
-        // TODO: show error message on inputs or notification
-        console.log("error:", err);
+      onError: (e: AxiosError<{ message: string }>) => {
+        const errorReceived = e.response?.data.message;
+        const incorrectPasswordErrMsg = new RegExp(/password is incorrect/i); // Full error: "This password is incorrect."
+        if (
+          errorReceived !== undefined &&
+          incorrectPasswordErrMsg.test(errorReceived)
+        ) {
+          form.setErrors({ current_password: errorReceived });
+          return;
+        }
+
+        // NOTE: shouldn't reach here unless client is deliberately overriding client-side validation. Or network error.
+        showSomethingWentWrongNotification();
       },
     }
   );
