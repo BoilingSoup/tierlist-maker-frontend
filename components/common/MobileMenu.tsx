455f16a7541844074b5e6d5568247f2d1ba6c087
BoilingSoup <boilingsoup@protonmail.com>
2023-04-29 12:41:50 -0700

    Update mobile nav styles

--- components/common/MobileMenu.tsx
+++ components/common/MobileMenu.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Flex } from "@mantine/core";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useRouterEvent } from "./hooks/useRouterEvent";
 import {
   getMobileNavLinkSx,
@@ -12,41 +13,44 @@ type Props = {
 };
 
 export const MobileMenu = ({ onLinkClick: closeMenu }: Props) => {
+  const router = useRouter();
+  const currentRoute = router.route;
+
   useRouterEvent({ on: "routeChangeComplete", handler: closeMenu });
 
   return (
     <Box sx={mobileNavLinksOverlaySx}>
       <Flex sx={mobileNavLinksContainerSx}>
         <Center
-          // sx={getMobileNavLinkSx(currentPath === Route.Home)}
+          sx={getMobileNavLinkSx(currentRoute === "/")}
           component={Link}
           href="/"
         >
           Home
         </Center>
         <Center
-          // sx={getMobileNavLinkSx(currentPath === Route.Browse)}
+          sx={getMobileNavLinkSx(currentRoute === "/browse")}
           component={Link}
           href="/browse"
         >
           Browse
         </Center>
         <Center
-          // sx={getMobileNavLinkSx(currentPath === Route.Create)}
+          sx={getMobileNavLinkSx(currentRoute === "/create")}
           component={Link}
           href="/create"
         >
           Create New Tier List
         </Center>
         <Center
-          // sx={getMobileNavLinkSx(currentPath === Route.Register)}
+          sx={getMobileNavLinkSx(currentRoute === "/register")}
           component={Link}
           href="/register"
         >
           Register
         </Center>
         <Center
-          // sx={getMobileNavLinkSx(currentPath === Route.SignIn)}
+          sx={getMobileNavLinkSx(currentRoute === "/signin")}
           component={Link}
           href="/signin"
         >
