b65555988be5d35193b97224b44c96d008e39d20
BoilingSoup <boilingsoup@protonmail.com>
2023-04-29 16:09:04 -0700

    Change mobile nav links when authenticated

--- components/common/MobileMenu.tsx
+++ components/common/MobileMenu.tsx
@@ -1,11 +1,15 @@
-import { Box, Center, Flex } from "@mantine/core";
+import { Box, Center, Flex, Loader } from "@mantine/core";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useAuth } from "../../contexts/AuthProvider";
+import { useSignOutMutation } from "../../hooks/api/useSignOutMutation";
 import { useRouterEvent } from "./hooks/useRouterEvent";
 import {
   getMobileNavLinkSx,
   mobileNavLinksContainerSx,
   mobileNavLinksOverlaySx,
+  mobileNavLinkSx,
+  mobileSignOutButtonSx,
 } from "./styles";
 
 type Props = {
@@ -13,8 +17,13 @@ type Props = {
 };
 
 export const MobileMenu = ({ onLinkClick: closeMenu }: Props) => {
+  const { user, isLoading } = useAuth();
   const router = useRouter();
   const currentRoute = router.route;
+  const isAuthenticated = !isLoading && !!user;
+  const isUnauthenticated = !isLoading && !user;
+
+  const { mutate: signOut, isLoading: isSigningOut } = useSignOutMutation();
 
   useRouterEvent({ on: "routeChangeComplete", handler: closeMenu });
 
@@ -42,20 +51,55 @@ export const MobileMenu = ({ onLinkClick: closeMenu }: Props) => {
         >
           Create New Tier List
         </Center>
-        <Center
-          sx={getMobileNavLinkSx(currentRoute === "/register")}
-          component={Link}
-          href="/register"
-        >
-          Register
-        </Center>
-        <Center
-          sx={getMobileNavLinkSx(currentRoute === "/signin")}
-          component={Link}
-          href="/signin"
-        >
-          Sign In
-        </Center>
+        {isLoading && (
+          <>
+            <Center sx={mobileNavLinkSx}>
+              <Loader color="cyan" size="sm" />
+            </Center>
+            <Center sx={mobileNavLinkSx}>
+              <Loader color="cyan" size="sm" />
+            </Center>
+          </>
+        )}
+        {isAuthenticated && (
+          <>
+            <Center
+              sx={getMobileNavLinkSx(
+                currentRoute === "/account/tierlists" ||
+                  currentRoute === "/account/settings"
+              )}
+              component={Link}
+              href="/account/tierlists"
+            >
+              Account
+            </Center>
+            <Center
+              sx={mobileSignOutButtonSx}
+              component="button"
+              onClick={() => signOut()}
+            >
+              {isSigningOut ? <Loader color="cyan" size="sm" /> : "Sign Out"}
+            </Center>
+          </>
+        )}
+        {isUnauthenticated && (
+          <>
+            <Center
+              sx={getMobileNavLinkSx(currentRoute === "/register")}
+              component={Link}
+              href="/register"
+            >
+              Register
+            </Center>
+            <Center
+              sx={getMobileNavLinkSx(currentRoute === "/signin")}
+              component={Link}
+              href="/signin"
+            >
+              Sign In
+            </Center>
+          </>
+        )}
       </Flex>
     </Box>
   );
