719f84abfdcdf356f173c4d57d1219b9c1f93143
BoilingSoup <boilingsoup@protonmail.com>
2023-05-24 11:28:43 -0700

    Fullscreen maximizes whole page

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,29 +1,18 @@
 import { DndContext, DragOverlay } from "@dnd-kit/core";
 import { Box, CSSObject, Flex, MantineTheme } from "@mantine/core";
-import {
-  useFullscreen as useFullScreen,
-  useViewportSize,
-} from "@mantine/hooks";
+import { useFullscreen as useFullScreen, useViewportSize } from "@mantine/hooks";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import { append } from "../../components/common/helpers";
 import { NAVBAR_HEIGHT } from "../../components/common/styles";
 import { initialData } from "../../components/tierlist/constants";
-import {
-  getDragHandlers,
-  getFullScreenProp,
-} from "../../components/tierlist/helpers";
+import { getDragHandlers, getFullScreenProp } from "../../components/tierlist/helpers";
 import { usePasteEvent } from "../../components/tierlist/hooks/usePasteEvent";
 import { OverlayImage } from "../../components/tierlist/image-area/OverlayImage";
 import { Sidebar } from "../../components/tierlist/Sidebar";
 import { TierListRow } from "../../components/tierlist/TierListRow";
-import {
-  ActiveItemState,
-  ClientSideImage,
-  PxSize,
-  TierListData,
-} from "../../components/tierlist/types";
+import { ActiveItemState, ClientSideImage, PxSize, TierListData } from "../../components/tierlist/types";
 import { SITE_NAME } from "../../config/config";
 
 const junk: CSSObject = {
@@ -54,9 +43,7 @@ const Create: NextPage = () => {
       })
     );
 
-  const { dragStartHandler, dragOverHandler, dragEndHandler } = getDragHandlers(
-    { data, setData, setActiveItem }
-  );
+  const { dragStartHandler, dragOverHandler, dragEndHandler } = getDragHandlers({ data, setData, setActiveItem });
 
   const isFullScreen = fullScreen.fullscreen;
 
@@ -69,27 +56,16 @@ const Create: NextPage = () => {
       <Head>
         <title>Create Tier List</title>
       </Head>
-      <DndContext
-        id={SITE_NAME}
-        onDragStart={dragStartHandler}
-        onDragOver={dragOverHandler}
-        onDragEnd={dragEndHandler}
-      >
-        <Flex ref={fullScreen.ref} sx={junk}>
+      <DndContext id={SITE_NAME} onDragStart={dragStartHandler} onDragOver={dragOverHandler} onDragEnd={dragEndHandler}>
+        <Flex sx={junk}>
           <Box sx={junk2}>
             {data.rows.map((row) => (
               <TierListRow key={row.id} data={row} height={rowPxHeight} />
             ))}
           </Box>
-          <Sidebar
-            fullScreen={getFullScreenProp(fullScreen)}
-            images={data.sidebar}
-            onAddImage={addImageHandler}
-          />
+          <Sidebar fullScreen={getFullScreenProp(fullScreen)} images={data.sidebar} onAddImage={addImageHandler} />
         </Flex>
-        <DragOverlay>
-          {activeItem ? <OverlayImage img={activeItem} /> : null}
-        </DragOverlay>
+        <DragOverlay>{activeItem ? <OverlayImage img={activeItem} /> : null}</DragOverlay>
       </DndContext>
     </>
   );
