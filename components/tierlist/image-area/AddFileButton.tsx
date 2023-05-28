4ddc124587c42f3069498b993f6129affcc3684b
BoilingSoup <boilingsoup@protonmail.com>
2023-05-28 10:27:14 -0700

    Style mobile add files button

--- components/tierlist/image-area/AddFileButton.tsx
+++ components/tierlist/image-area/AddFileButton.tsx
@@ -1,7 +1,8 @@
-import { Button, FileButton } from "@mantine/core";
+import { Button, FileButton, Stack, Text } from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
 import { nanoid } from "nanoid";
 import { useRef } from "react";
+import { useIsDesktopScreen } from "../../common/hooks/useIsDesktopScreen";
 import { addFileButtonSx } from "../styles";
 import { ClientSideImage } from "../types";
 
@@ -11,6 +12,7 @@ type Props = {
 
 export const AddFileButton = ({ onAddImage: setImageSources }: Props) => {
   const resetRef = useRef<() => void>(null);
+  const isDesktopScreen = useIsDesktopScreen();
 
   const addFileHandler = (files: File[]) => {
     const newImages: ClientSideImage[] = files.map((file) => ({
@@ -28,7 +30,19 @@ export const AddFileButton = ({ onAddImage: setImageSources }: Props) => {
     <FileButton resetRef={resetRef} onChange={addFileHandler} accept="image/png,image/jpeg,image/webp" multiple>
       {(props) => (
         <Button {...props} sx={addFileButtonSx}>
-          <IconPlus /> Add Files
+          {isDesktopScreen ? (
+            <>
+              <IconPlus />
+              Add Files
+            </>
+          ) : (
+            <Stack>
+              <IconPlus size="50" style={{ margin: "auto" }} />
+              <Text component="span" size="lg">
+                Add Files
+              </Text>
+            </Stack>
+          )}
         </Button>
       )}
     </FileButton>
