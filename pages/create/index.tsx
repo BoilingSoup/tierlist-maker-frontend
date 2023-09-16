1ecd52788b85c0e63fcfdf6cd431c3d5fb57dd29
BoilingSoup <boilingsoup@protonmail.com>
2023-09-15 22:00:55 -0700

    Export png saves tier list as .png image

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import { append, pxToNumber } from "../../components/common/helpers";
 import { useIsDesktopScreen } from "../../components/common/hooks/useIsDesktopScreen";
 import { NAVBAR_HEIGHT } from "../../components/common/styles";
-import { initialData } from "../../components/tierlist/constants";
+import { DOM_TO_PNG_ID, initialData } from "../../components/tierlist/constants";
 import { getDragHandlers, getFullScreenProp, getRowHandlers } from "../../components/tierlist/helpers";
 import { useDndSensors } from "../../components/tierlist/hooks/useDndSensors";
 import { usePasteEvent } from "../../components/tierlist/hooks/usePasteEvent";
@@ -81,23 +81,25 @@ const Create: NextPage = () => {
         sensors={sensors}
       >
         <Flex sx={createPageMainContainerSx}>
-          <Box sx={rowsContainerSx} ref={animateChildren}>
-            {data.rows.map((row) => (
-              <TierListRow
-                key={row.id}
-                data={row}
-                minHeight={minHeight}
-                deletable={data.rows.length <= 1}
-                onMoveUp={handleMoveRowUp}
-                onMoveDown={handleMoveRowDown}
-                onChangeLabel={handleChangeLabel}
-                onChangeColor={handleChangeColor}
-                onAddRowAbove={handleAddRowAbove}
-                onAddRowBelow={handleAddRowBelow}
-                onDeleteRow={handleDeleteRow}
-                onClearRow={handleClearRow}
-              />
-            ))}
+          <Box sx={rowsContainerSx}>
+            <Box ref={animateChildren} id={DOM_TO_PNG_ID} bg="dark.7">
+              {data.rows.map((row) => (
+                <TierListRow
+                  key={row.id}
+                  data={row}
+                  minHeight={minHeight}
+                  deletable={data.rows.length <= 1}
+                  onMoveUp={handleMoveRowUp}
+                  onMoveDown={handleMoveRowDown}
+                  onChangeLabel={handleChangeLabel}
+                  onChangeColor={handleChangeColor}
+                  onAddRowAbove={handleAddRowAbove}
+                  onAddRowBelow={handleAddRowBelow}
+                  onDeleteRow={handleDeleteRow}
+                  onClearRow={handleClearRow}
+                />
+              ))}
+            </Box>
           </Box>
           <Sidebar fullScreen={getFullScreenProp(fullScreen)} images={data.sidebar} onAddImage={handleAddImage} />
         </Flex>
