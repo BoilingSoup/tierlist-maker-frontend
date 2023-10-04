f7035beaa1e7d117e72a1d5414a2063a2957c1c9
BoilingSoup <boilingsoup@protonmail.com>
2023-10-04 13:21:25 -0700

    Change browse page background for morale

--- pages/browse/index.tsx
+++ pages/browse/index.tsx
@@ -1,10 +1,19 @@
+import { Box } from "@mantine/core";
 import type { NextPage } from "next";
+import { NAVBAR_HEIGHT } from "../../components/common/styles";
 
 const Browse: NextPage = () => {
   return (
-    <>
-      <div>Browse page...</div>
-    </>
+    <Box
+      sx={(theme) => ({
+        background: theme.colors.dark[7],
+        borderTop: `1px solid ${theme.colors.dark[3]}`,
+        height: `calc(100vh - ${NAVBAR_HEIGHT})`,
+        color: "white",
+      })}
+    >
+      Browse page...
+    </Box>
   );
 };
 
