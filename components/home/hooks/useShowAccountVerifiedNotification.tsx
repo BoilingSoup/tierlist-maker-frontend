dcc82e379f3b41175a0e109816115c06c92a3366
BoilingSoup <84747244+BoilingSoup@users.noreply.github.com>
2023-10-10 11:40:50 -0700

    Tweak css (#217)
    * Update styles
    
    * Show verification notification once only

--- components/home/hooks/useShowAccountVerifiedNotification.tsx
+++ components/home/hooks/useShowAccountVerifiedNotification.tsx
@@ -1,15 +1,17 @@
 import { MantineTheme } from "@mantine/core";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { showSuccessNotification } from "../../common/helpers";
 
 export const useShowAccountVerifiedNotification = (theme: MantineTheme) => {
+  const [shown, setShown] = useState(false);
   useEffect(() => {
-    if (window.location.href.includes("?verified=1")) {
+    if (window.location.href.includes("?verified=1") && !shown) {
       showSuccessNotification({
         theme,
         title: "Verified",
         message: "Your account has been verified.",
       });
+      setShown(true);
     }
-  }, [theme]);
+  }, [theme, shown]);
 };
